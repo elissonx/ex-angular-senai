50bfbbcc36ec03fa4146d8c29cf5148a1a392131
elissonx <elisson.souza@hotmail.com>
2021-11-01 16:29:21 -0300

    exangularsenai

---
+++ ExAngularSenai/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './views/contato/contato.component';
+
+const routes: Routes = [
+  { path: "contato", component: ContatoComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
